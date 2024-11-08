fc0c75321821e1038e070e123d200d01b26577ec
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-11-08 20:55:25 +0300

    done

--- src/components/ColorChanger.tsx
+++ src/components/ColorChanger.tsx
@@ -1,16 +1,43 @@
 import { useState } from "react";
 
+interface T {
+  value: string;
+  message: string | null
+}
+
 export const ColorChanger = () => {
 
-  const[form, setForm] = useState({
-    value: '#562232'
+  const[form, setForm] = useState<T>({
+    value: '#E74C3C',
+    message: 'Ошибка!'
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
- 
     const { value } = e.target;
-    setForm({value})
-    console.log(value)
+
+    const hexToRgb = (value: string) => {
+      const hexRegex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i;
+      const result = hexRegex.exec(value);
+      return result 
+        ? [ parseInt(result[1], 16),
+            parseInt(result[2], 16),
+            parseInt(result[3], 16) ]
+        : null;
+    }
+
+    const newValue = hexToRgb(value); 
+
+    if(value.length === 7 && newValue) {
+      setForm({
+        value: value,
+        message: `rgb(${newValue[0]}, ${newValue[1]}, ${newValue[2]})`
+      });
+    } else {
+      setForm({
+        value: '#E74C3C',
+        message: 'Ошибка!'
+    })
+    }
   };
 
   return (
@@ -21,7 +48,7 @@ export const ColorChanger = () => {
           <input type="text" id="color" name="color" onChange={handleChange}/>
         </form>
         <div className="textWrapper">
-          <span>{form.value}</span>
+          <span>{form.message}</span>
         </div>
       </div>
     </div>
